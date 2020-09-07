0c4868a5582db767c552eac782716eeb3edb35cf
Calvin2412 <2119777@talnet.nl>
2020-09-07 22:45:49 +0200

    opdracht 6

---
+++ lostpsw.php
@@ -0,0 +1,18 @@
+<?php
+<html>
+
+<h1>Wachtwoord vergeten</h1>
+
+<body>
+
+<form>
+<input type="text" name="E-mail" placeholder="E-mail"><br>
+
+
+<button type="submit" class="button">Verzenden</button>
+</form>
+
+</body>
+
+</html>
+?>
