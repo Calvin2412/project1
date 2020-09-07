4c8d4a3e9d743272783473fb63e3ca9172565cf9
Calvin2412 <2119777@talnet.nl>
2020-09-07 22:39:01 +0200

    opdracht 4

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+<html>
+
+<h1>Log in</h1>
+
+<body>
+
+<form>
+<input type="text" name="Gebruikersnaam" placeholder="Gebruikersnaam"><br>
+<input type="password" name="Wachtwoord" placeholder="Wachtwoord"><br>
+
+
+<button type="submit" class="button">Inloggen</button>
+
+<a class="nieuw account aanmaken" href="http://localhost/project1/signup.php">Maak een nieuw account aan</a><br>
+<a class="wachtwoord vergeten" href="http://localhost/project1/lostpsw.php">Wachtwoord vergeten</a>
+</form>
+
+</body>
+
+</html>
+?>
---
+++ signup.php
@@ -0,0 +1,22 @@
+<?php
+<html>
+
+<h1>Aanmelden</h1>
+
+<body>
+
+<form>
+<input type="text" name="Voornaam" placeholder="Voornaam"><br>
+<input type="text" name="Achternaam" placeholder="Achternaam"><br>
+<input type="text" name="E-mail" placeholder="E-mail"><br>
+<input type="text" name="Gebruikersnaam" placeholder="Gebruikersnaam"><br>
+<input type="text" name="Wachtwoord" placeholder="Wachtwoord"><br>
+<input type="text" name="Herhaal wachtwoord" placeholder="Herhaal wachtwoord"><br>
+
+<button type="submit" class="button">Maak een account aan</button>
+</form>
+
+</body>
+
+</html>
+?>
